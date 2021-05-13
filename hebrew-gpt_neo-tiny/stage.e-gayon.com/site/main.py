6a29690fa88aa732fc52f2b9b0f371cb19e230ec
Doron Adler <doron@glidetalk.com>
2021-05-13 22:02:36 +0300

    checkpoint -> timestamp Use gunicorn

--- hebrew-gpt_neo-tiny/stage.e-gayon.com/site/main.py
+++ hebrew-gpt_neo-tiny/stage.e-gayon.com/site/main.py
@@ -17,9 +17,9 @@ def favicon():
 def robots():
     return send_file('robots.txt', mimetype='text/plain')
 
-@app.route('/checkpoint')
+@app.route('/timestamp')
 def checkpoint():
-    return ("20210512-1729")
+    return ("20210513-2202")
     #return send_file('hebrew-gpt_neo-tiny/counter', mimetype='text/plain')
 
 @app.route('/', methods=['GET', 'POST'])
