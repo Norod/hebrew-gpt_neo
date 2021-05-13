c9ac70f4b892d400eee7fb232ec1f4347dd540be
Doron Adler <doron@glidetalk.com>
2021-05-13 23:36:43 +0300

    shira.e-gayon.com

---
+++ hebrew-gpt_neo-small/shira.e-gayon.com/site/main.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+from flask import Flask, request, send_file
+from model import extend
+
+maxNumOfTokensToExtend = 92
+
+app = Flask(__name__)
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_file('favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+@app.route('/robots.txt')
+def robots():
+    return send_file('robots.txt', mimetype='text/plain')
+
+@app.route('/timestamp')
+def checkpoint():
+    return ("20210513-2321")
+    #return send_file('hebrew-gpt_neo-tiny/counter', mimetype='text/plain')
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        return send_file('index.html')
+    
+    data = request.form.get('text_in')
+    if len(data) > 0:
+        title_data = str(data)
+    else:
+        title_data = ""
+    
+    return extend(title_data,maxNumOfTokensToExtend)
+
+
+if __name__ == "__main__":
+    app.run('0.0.0.0', os.environ.get('PORT', 8080))
---
+++ hebrew-gpt_neo-small/shira.e-gayon.com/site/start_local_debug.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+export LANG=C.UTF-8 
+export FLASK_APP=main.py
+export FLASK_ENV=development
+
+flask run
+
