14f973eac6b597884dadc542cd31cd5e9e189751
Doron Adler <doron@glidetalk.com>
2021-05-13 21:42:05 +0300

    checkpoint -> timestamp

--- hebrew-gpt_neo-tiny/tiny.e-gayon.com/site/main.py
+++ hebrew-gpt_neo-tiny/tiny.e-gayon.com/site/main.py
@@ -17,9 +17,9 @@ def favicon():
 def robots():
     return send_file('robots.txt', mimetype='text/plain')
 
-@app.route('/checkpoint')
+@app.route('/timestamp')
 def checkpoint():
-    return ("20210513-1654")
+    return ("20210513-2141")
     #return send_file('hebrew-gpt_neo-tiny/counter', mimetype='text/plain')
 
 @app.route('/', methods=['GET', 'POST'])
