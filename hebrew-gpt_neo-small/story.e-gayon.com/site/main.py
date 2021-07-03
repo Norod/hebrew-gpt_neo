a574593489a82a0d6c1f45f69ac15c121bdc1f32
Doron Adler <doron@glidetalk.com>
2021-07-03 16:57:17 +0300

    20210703-1649

--- hebrew-gpt_neo-small/story.e-gayon.com/site/main.py
+++ hebrew-gpt_neo-small/story.e-gayon.com/site/main.py
@@ -19,7 +19,7 @@ def robots():
 
 @app.route('/timestamp')
 def checkpoint():
-    return ("20210701-1243")
+    return ("20210703-1649")
     #return send_file('hebrew-gpt_neo-tiny/counter', mimetype='text/plain')
 
 @app.route('/', methods=['GET', 'POST'])
--- hebrew-gpt_neo-small/story.e-gayon.com/site/model.py
+++ hebrew-gpt_neo-small/story.e-gayon.com/site/model.py
@@ -27,7 +27,7 @@ model.to(device)
 
 def extend(input_text, max_size=20):
     if len(input_text) == 0:
-        input_text = "האיש האחרון בעולם ישב לבד בחדרו, לפתע נשמע נקישה"
+        input_text = ""
 
     encoded_prompt = tokenizer.encode(
     input_text, add_special_tokens=False, return_tensors="pt")
